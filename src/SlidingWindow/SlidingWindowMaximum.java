17bb4aa484a0ca0c9fe6f39ec760a9c0175db985
devtechtitan002-alt <dev.techtitan.002@gmail.com>
2026-03-06 17:07:45 +0530

    Leetcode-239-SlindingWindowMaximum

---
+++ src/SlidingWindow/SlidingWindowMaximum.java
@@ -0,0 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+public class SlidingWindowMaximum {
+    static class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> q = new ArrayDeque<>();
+        int[] res = new int[(nums.length-k)+1];
+        int idx=0,left=0;
+        for(int right=0;right<nums.length;right++){
+            while(!q.isEmpty() && nums[right] > nums[q.peekLast()]){
+                q.pollLast();
+            }
+            q.addLast(right);
+            if(right>=k-1){
+               res[idx++]=nums[q.peekFirst()];
+               left++;
+               if(q.peekFirst()<left) q.pollFirst();
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3)));
+        System.out.println(Arrays.toString(sol.maxSlidingWindow(new int[]{1,3,1,2,0,5}, 3)));
+    }
+}
+}
